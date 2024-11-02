6fa373028617bdc2eaa3553c0cc1556b34acd230
유태윤 <0704yutae@gmail.com>
2024-11-02 11:46:10 +0900

    While 예제 추가

--- practice/06. While-statementet..py
+++ practice/06. While-statementet..py
@@ -11,3 +11,19 @@ elif num > 60:
     print("B반입니다")
 else:
     print("C반입니다")
+
+
+print("===========================")
+
+count = int(input("숫자 하나를 입력하세요 >>"))
+i  = 0
+
+while i < count:
+    print("*")
+    i = i + 1
+
+
+import random
+value = random.randrange(1,100)
+print(value)
+
