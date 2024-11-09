3bbb6e089009da91debe2e1118e72af523efa28b
유태윤 <0704yutae@gmail.com>
2024-11-09 11:30:23 +0900

    while 실습

--- practice/06. While-statementet..py
+++ practice/06. While-statementet..py
@@ -27,3 +27,23 @@ import random
 value = random.randrange(1,100)
 print(value)
 
+# 100 미만의 피보나치 수열 출력하기
+
+"""
+fibonacci = 1
+n = 0
+m = 0
+
+while fibonacci < 100:
+    print(fibonacci)
+    m = n
+    n = fibonacci
+    fibonacci = m + n
+"""
+
+fibonacci_list = {1,1}
+n = 1
+
+while fibonacci_list[n-1]+fibonacci_list[n] < 100:
+    fibonacci_list.append(fibonacci_list[n-1]+fibonacci_list[n])
+    n = n+1
