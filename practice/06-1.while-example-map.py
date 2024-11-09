00271093ca2c3aa3ff6898e865d3ee00b0646e6c
유태윤 <0704yutae@gmail.com>
2024-11-09 11:31:18 +0900

    while, if 사용한 만들기

---
+++ practice/06-1.while-example-map.py
@@ -0,0 +1,15 @@
+x=0
+y=0
+
+if command = "up":
+    y <100
+    y+1 = y
+elif command = "down":
+    y > 0
+    y-1 = y
+elif command = "lift":
+    x < 100
+    x-1 100
+elif command = "right":
+    x > 0
+    x+1 = x
