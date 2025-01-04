0cc988a774c889129701464ab22c6e6a17982729
유태윤 <0704yutae@gmail.com>
2025-01-04 10:32:50 +0900

    for map 예제

---
+++ practice/07-1. for example map.py
@@ -0,0 +1,58 @@
+user_input = ""
+X = 0
+Y = 0
+
+valid_inputs = ["EXIT", "UP", "DOWN", "LEFT","RIGHT"]
+
+while user_input != "EXIT":
+    user_input = ""  # user_input 초기화
+    while user_input not in valid_inputs:
+        user_input = input("움직일 방향을 입력하세요 (종료는 EXIT) :: ")
+
+    if user_input == "UP":
+        if Y < 10:
+            Y = Y+1
+        else:
+            print("map의 범위를 벗어날 수 없습니다.")
+    elif user_input == "DOWN":
+        if Y > 0:
+            Y = Y-1
+        else:
+            print("map의 범위를 벗어날 수 없습니다.")
+    elif user_input == "LEFT":
+        if X>0:
+            X = X-1
+        else:
+            print("map의 범위를 벗어날 수 없습니다.")
+    elif user_input == "RIGHT":
+        if X < 10:
+            X = X+1
+        else:
+            print("map의 범위를 벗어날 수 없습니다.")
+    print("현재 위치 : (", X, ",", y,")")
+print("프로그램을 종료합니다.")
+
+##########
+#********#
+#********#
+#********#
+#********#
+#********#
+#********#
+#********#
+#*@******#
+##########
+
+
+
+
+
+
+
+
+
+
+
+
+
+
