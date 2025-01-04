76c07ab938631f800e90f7b72fcbe13c74881d38
유태윤 <0704yutae@gmail.com>
2025-01-04 10:46:42 +0900

    for map 예제

--- practice/07-1. for example map.py
+++ practice/07-1. for example map.py
@@ -1,36 +1,52 @@
 user_input = ""
+
 X = 0
+
 Y = 0
 
-valid_inputs = ["EXIT", "UP", "DOWN", "LEFT","RIGHT"]
+directions = ["UP", "DOWN", "LEFT", "RIGHT"]
+
+dx = [0, 0, -1, 1]
+
+dy = [1, -1, 0, 0]
+
+valid_inputs = ["EXIT"] + directions
 
 while user_input != "EXIT":
+
     user_input = ""  # user_input 초기화
+
     while user_input not in valid_inputs:
         user_input = input("움직일 방향을 입력하세요 (종료는 EXIT) :: ")
 
-    if user_input == "UP":
-        if Y < 10:
-            Y = Y+1
-        else:
+    if user_input in directions:
+
+        index = directions.index(user_input)
+
+        tmpX = X + dx[index]
+
+        if tmpX > 10 or tmpX < 0:
+
             print("map의 범위를 벗어날 수 없습니다.")
-    elif user_input == "DOWN":
-        if Y > 0:
-            Y = Y-1
+
         else:
+
+            X = tmpX
+
+        tmpY = Y + dy[index]
+
+        if tmpY > 10 or tmpY < 0:
+
             print("map의 범위를 벗어날 수 없습니다.")
-    elif user_input == "LEFT":
-        if X>0:
-            X = X-1
+
         else:
-            print("map의 범위를 벗어날 수 없습니다.")
-    elif user_input == "RIGHT":
-        if X < 10:
-            X = X+1
-        else:
-            print("map의 범위를 벗어날 수 없습니다.")
-    print("현재 위치 : (", X, ",", y,")")
+
+            Y = tmpY
+
+    print("현재 위치 : (", X, ",", Y, ")")
+
 print("프로그램을 종료합니다.")
+
 
 ##########
 #********#
